2ac1fa8fe8aad2706da840ca6b71734f9e8a2c68
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2025-05-23 20:25:33 -0300

    Feat: Captura do Video pelo cv2

--- src/projeto/main.py
+++ src/projeto/main.py
@@ -1,7 +1,24 @@
-# Ponto de entrada do pacote
+import cv2
+import numpy as np
+
+VIDEO = "./src/videos/cars.mp4"
+# ARQUIVO_MODELO = 'models/frozen_inference_graph.pb'
+# ARQUIVO_CFG = 'models/ssd_mobilenet_v2_coco.pbtxt'
+# ARQUIVO_LABELS = 'models/coco_labels.txt'
+
 
 def main():
-    print("Executando o programa principal")
+    cap = cv2.VideoCapture(VIDEO)
+    while True:
+        _, frame = cap.read()
+
+        cv2.imshow("frame", frame)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
