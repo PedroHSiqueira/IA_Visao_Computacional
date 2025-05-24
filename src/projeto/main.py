4e49cb4dab79040c2cad437ef238f407a9730da1
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2025-05-24 00:39:35 -0300

    Fix: Resize da imagem

--- src/projeto/main.py
+++ src/projeto/main.py
@@ -6,7 +6,10 @@ VIDEO = "./src/videos/cars.mp4"
 def main():
     cap = cv2.VideoCapture(VIDEO)
     while True:
-        _, frame = cap.read()
+        check, frame = cap.read()
+        if not check:
+            break
+        frame = cv2.resize(frame, (640, 480))
 
         cv2.imshow("frame", frame)
         key = cv2.waitKey(1)
